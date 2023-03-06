a33afeb3ecadd22a36ce60ffeb1e987e2b5d72b5
bloxar <flyingpineappleman@gmail.com>
2023-03-06 13:33:45 +1000

    basic GUI menus implemented

--- App/Editor.py
+++ App/Editor.py
@@ -1,11 +1,74 @@
 from json import loads, dumps
 import base64
+from os.path import splitext
 
 
 class Savedata():
     """
     Contains savedata as a dictionary.
     """
+
+    # all valid fields and their types.
+    # types are a sequence of any of str, num, list, map.
+    # list takes 1 arg, map takes 2
+    fields = {"badass": ["num"],
+              "bosses": ["map", "num", "list", "num"],
+              "bossGearbits": ["list", "str"],
+              "cape": ["num"],
+              "cCapes": ["list", "num"],
+              "CH": ["num"],
+              "charDeaths": ["num"],
+              "checkAmmo": ["num"],
+              "checkBat": ["num"],
+              "checkCID": ["num"],
+              "checkHP": ["num"],
+              "checkRoom": ["num"],
+              "checkStash": ["num"],
+              "checkX": ["num"],
+              "checkY": ["num"],
+              "cl": ["map", "num", "list", "num"],
+              "compShell": ["num"],
+              "cShells": ["list", "num"],
+              "cSwords": ["list", "num"],
+              "cues": ["list", "num"],
+              "dateTime": ["num"],
+              "destruct": ["map", "num", "list", "num"],
+              "drifterkey": ["num"],
+              "enemies": ["map", "num", "list", "num"],
+              "eq00": ["num"],
+              "eq01": ["num"],
+              "events": ["list", "num"],
+              "fireplaceSave": ["num"],
+              "gameName": ["str"],
+              "gear": ["num"],
+              "gearReminderTimes": ["num"],
+              "gunReminderTimes": ["num"],
+              "halluc": ["num"],
+              "hasMap": ["num"],
+              "healthKits": ["list", "num"],
+              "healthUp": ["num"],
+              "mapMod": ["map", "num", "num"],
+              "newcomerHoardeMessageShown": ["num"],
+              "noSpawn": ["list", "num"],
+              "noviceMode": ["num"],
+              "permaS": ["map", "num", "num"],
+              "playT": ["num"],
+              "rooms": ["list", "num"],
+              "sc": ["list", "num"],
+              "scUp": ["list", "num"],
+              "scK": ["map", "num", "num"],
+              "skill": ["list", "num"],
+              "specialUp": ["num"],
+              "successfulCollectTimes": ["num"],
+              "successfulHealTimes": ["num"],
+              "successfulWarpTimes": ["num"],
+              "sword": ["num"],
+              "tablet": ["list", "num"],
+              "tutHeal": ["num"],
+              "values": ["map", "str", "num"],
+              "warp": ["list", "num"],
+              "well": ["list", "num"],
+              "wellMap": ["list", "num"]}
 
     def __init__(self, data):
         self.savedata_map = data
@@ -14,14 +77,40 @@ class Savedata():
         return self.savedata_map.get(field)
 
     def set_field(self, field, value):
-        # TODO - check if field is a valid string
-        self.savedata_map[field] = value
+        if (field in fields):
+            self.savedata_map[field] = value
+
+    # parse data from .hlds file
+    def parse_hlds(hldsdata):
+        savedata_map = {1:2} #placeholder
+        # TODO - parse hlds
+        return savedata_map
+
+    # save data to .hlds file
+    def save_hlds(filename):
+        pass
+        # TODO - save hlds
 
     # parse data from .sav file
     def parse_savefile(jsondata):
-        savedata_map = {}
+        savedata_map = loads(jsondata)
         # TODO - convert jsondata to fields and add to savedata_map
+        for key, value in savedata_map.items():
+            fieldtype = Savedata.fields[key]
+            if len(fieldtype) > 1:
+                savedata_map[key] = Savedata.parse_savedata_collection(value, fieldtype)
+        print(savedata_map)
         return savedata_map
+
+    # export data to .sav file
+    def export_savefile(filename):
+        pass
+
+
+    # convert list/map in savedata format to list/map object
+    def parse_savedata_collection(raw, type):
+        # TODO !!!
+        return None
 
 
 class Editor():
@@ -33,20 +122,32 @@ class Editor():
 
     def __init__(self, savefile_path):
         self.path = savefile_path
-        self.filename = "No File"
+        self.filename = None # name of loaded .hlds file
         self.savedata = None
 
     def load(self, filename):
-        # read file
-        savefile = open(filename, "rb", buffering=0)
-        jsondata = base64.standard_b64decode(savefile.read())[60:-1].decode()
-        # create savedata object
-        self.savedata = Savedata(Savedata.parse_savefile(jsondata))
+        ext = splitext(filename)[1]
+        if ext == ".hlds":
+            savefile = open(filename, "rt")
+            self.savedata = Savedata(Savedata.parse_hlds(savefile.read()))
+            self.filename = filename
+        elif ext == ".sav":
+            savefile = open(filename, "rb", buffering=0)
+            jsondata = base64.standard_b64decode(savefile.read())[60:-1].decode()
+            self.savedata = Savedata(Savedata.parse_savefile(jsondata))
+        elif ext == "":
+            return
+        else:
+            raise FileNotFoundError("Invald file type")
+        
 
     def save(self, filename=None):
-        if filename is None:
-            filename = self.filename
-        pass
+        savefile = open(filename, "wt")
+        # TODO - write hlds
+
+    def export(self, slot):
+        print(slot)
+        # TODO
 
     def __str__(self):
         if self.savedata:
--- App/Interface.py
+++ App/Interface.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter.messagebox import showerror
 
 class Interface():
     """
@@ -8,38 +9,65 @@ class Interface():
     """
 
     # file -> load
-    def load(self):
-        filename = filedialog.askopenfilename(initialdir=self.app.savefile_path)
-        self.editor.load(filename)
+    def load(self, event=None):
+        try:
+            filename = filedialog.askopenfilename(initialdir=self.app.savefile_path)
+            self.editor.load(filename)
+        except Exception as e:
+            showerror(title=type(e), message=e)
 
 
     # file -> save
-    def save(self):
-        pass
+    def save(self, event=None):
+        try:
+            filename = self.editor.filename
+            if filename is None:
+                filename = filedialog.asksaveasfilename(initialdir=self.app.savefile_path, defaultextension=".hlds", filetypes=[("HLDS File", "*.hlds")])
+            self.editor.save(filename)
+        except Exception as e:
+            showerror(title="Error", message=e)
 
 
     # file -> save as
     def saveas(self):
-        pass
+        try:
+            filename = filedialog.asksaveasfilename(initialdir=self.app.savefile_path, defaultextension=".hlds", filetypes=[("HLDS File", "*.hlds")])
+            self.editor.save(filename)
+        except Exception as e:
+            showerror(title="Error", message=e)
 
     
     # file -> export
-    def export(self):
-        pass
+    def export(self, slot):
+        self.editor.export(slot)
     
 
     def __init__(self, editor, app):
         self.editor = editor
         self.app = app
         self.tk = Tk(screenName="Editor")
+        self.tk.option_add('*tearOff', FALSE)
         
         # menu
         self.menu = Menu(self.tk)
-        self.filemenu = Menu(self.menu, tearoff=0)
+        self.filemenu = Menu(self.menu)
+        self.exportmenu = Menu(self.filemenu)
         self.menu.add_cascade(label="File", menu=self.filemenu)
-        self.filemenu.add_command(label="Load", command=self.load)
-        self.filemenu.add_command(label="Save", command=self.save)
+
+        self.filemenu.add_command(label="Load", command=self.load, accelerator="Ctrl+L")
+        self.filemenu.add_command(label="Save", command=self.save, accelerator="Ctrl+S")
         self.filemenu.add_command(label="Save As", command=self.saveas)
-        self.filemenu.add_command(label="Export", command=self.export)
+        self.filemenu.add_cascade(label="Export", menu=self.exportmenu)
+        self.exportmenu.add_command(label="Slot 1", command=lambda: self.export(1), accelerator="Ctrl+1")
+        self.exportmenu.add_command(label="Slot 2", command=lambda: self.export(2), accelerator="Ctrl+2")
+        self.exportmenu.add_command(label="Slot 3", command=lambda: self.export(3), accelerator="Ctrl+3")
+        self.exportmenu.add_command(label="Slot 4", command=lambda: self.export(4), accelerator="Ctrl+4")
+
+        self.tk.bind("<Control-KeyPress-1>", lambda x: self.export(1))
+        self.tk.bind("<Control-KeyPress-2>", lambda x: self.export(2))
+        self.tk.bind("<Control-KeyPress-3>", lambda x: self.export(3))
+        self.tk.bind("<Control-KeyPress-4>", lambda x: self.export(4))
+        self.tk.bind("<Control-l>", self.load)
+        self.tk.bind("<Control-s>", self.save)
         self.tk.config(menu=self.menu)
 
